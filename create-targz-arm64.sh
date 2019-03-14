fd3923605283a6f802d6d60a485813b6995f5574
Hayden <hayden@whitewaterfoundry.com>
2019-03-14 18:49:59 -0400

    Have arm64 build upgrade to testing

--- create-targz-arm64.sh
+++ create-targz-arm64.sh
@@ -12,9 +12,6 @@ cd $TMPDIR
 sudo apt-get -y -q update
 sudo apt-get -y -q install curl gnupg cdebootstrap qemu-user-static
 
-# the workaround below in the x86 version does not work when building the arm64 version because of a qemu dependency
-# so instead we are building as stable and then upgrading to testing
-
 # bootstrap image
 sudo cdebootstrap -a $ARCH --foreign --include=sudo,locales,git,ssh,gnupg,apt-transport-https,wget,ca-certificates,man,less,curl,bash-completion,vim $DIST $DIST http://deb.debian.org/debian
 
@@ -25,6 +22,8 @@ sudo chroot $DIST apt clean
 sudo curl "https://salsa.debian.org/rhaist-guest/WSL/raw/master/linux_files/profile" -so "${TMPDIR}/${DIST}/etc/profile"
 sudo cp $BUILDIR/linux_files/setup $TMPDIR/$DIST/etc/setup
 sudo chroot $DIST /bin/bash -c "bash /etc/setup --silent"
+sudo chroot $DIST apt-get update
+sudo chroot $DIST apt-get -y -q dist-upgrade
 
 # configure initial language settings
 sudo chroot $DIST /bin/bash -c "echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen && locale-gen"
