8e7643b1bbd7deca719ecbf687232c6904c1e94c
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2019-12-10 10:27:55 +0100

    Add sudo

--- switch2dev.sh
+++ switch2dev.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 echo -n "Installing apt-transport-https... "
-apt-get install -y apt-transport-https &> /dev/null
+sudo apt-get install -y apt-transport-https &> /dev/null
 echo "done."
 
-sed -i 's$/pengwin-setup/$/pengwin-setup-dev/$g' /etc/apt/sources.list.d/whitewaterfoundry.list
-sed -i 's$/pengwin-base/$/pengwin-base-dev/$g' /etc/apt/sources.list.d/whitewaterfoundry.list
+sudo sed -i 's$/pengwin-setup/$/pengwin-setup-dev/$g' /etc/apt/sources.list.d/whitewaterfoundry.list
+sudo sed -i 's$/pengwin-base/$/pengwin-base-dev/$g' /etc/apt/sources.list.d/whitewaterfoundry.list
 
 echo -n "Importing packagecloud gpg key... "
 # import the gpg key
-curl -L "https://packagecloud.io/whitewaterfoundry/pengwin-setup-dev/gpgkey" 2> /dev/null | apt-key add - &>/dev/null
-curl -L "https://packagecloud.io/whitewaterfoundry/pengwin-base-dev/gpgkey" 2> /dev/null | apt-key add - &>/dev/null
+sudo curl -L "https://packagecloud.io/whitewaterfoundry/pengwin-setup-dev/gpgkey" 2> /dev/null | apt-key add - &>/dev/null
+sudo curl -L "https://packagecloud.io/whitewaterfoundry/pengwin-base-dev/gpgkey" 2> /dev/null | apt-key add - &>/dev/null
 echo "done."
 
 echo -n "Running apt-get update... "
 # update apt on this system
-apt-get update &> /dev/null
+sudo apt-get update -y &> /dev/null
+sudo apt-get upgrade -y 
 echo "done."
 
 echo
