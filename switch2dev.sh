94120edf0b3ede051373eef3da78b91cd1dcca7d
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2019-12-17 14:56:39 +0100

    Correctly some public key issues

--- switch2dev.sh
+++ switch2dev.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-echo -n "Installing apt-transport-https... "
-sudo apt-get install -y apt-transport-https &> /dev/null
-echo "done."
+if [[ "$1" == "release" ]]; then
+  export OLD='-dev'
+  export NEW=''
+else
+  export OLD=''
+  export NEW='-dev'
+fi
 
-sudo sed -i 's$/pengwin-setup/$/pengwin-setup-dev/$g' /etc/apt/sources.list.d/whitewaterfoundry.list
-sudo sed -i 's$/pengwin-base/$/pengwin-base-dev/$g' /etc/apt/sources.list.d/whitewaterfoundry.list
+curl -s "https://packagecloud.io/install/repositories/whitewaterfoundry/pengwin-base${NEW}/script.deb.sh" | sudo env os=debian dist=buster bash
+curl -s "https://packagecloud.io/install/repositories/whitewaterfoundry/pengwin-setup${NEW}/script.deb.sh" | sudo env os=debian dist=buster bash
 
-echo -n "Importing packagecloud gpg key... "
-# import the gpg key
-sudo curl -L "https://packagecloud.io/whitewaterfoundry/pengwin-setup-dev/gpgkey" 2> /dev/null | apt-key add - &>/dev/null
-sudo curl -L "https://packagecloud.io/whitewaterfoundry/pengwin-base-dev/gpgkey" 2> /dev/null | apt-key add - &>/dev/null
-echo "done."
+sudo sed -i "s\$/pengwin-setup${OLD}/\$/pengwin-setup${NEW}/\$g" /etc/apt/sources.list.d/whitewaterfoundry.list
+sudo sed -i "s\$/pengwin-base${OLD}/\$/pengwin-base${NEW}/\$g" /etc/apt/sources.list.d/whitewaterfoundry.list
 
-echo -n "Running apt-get update... "
-# update apt on this system
-sudo apt-get update -y &> /dev/null
-sudo apt-get upgrade -y 
-echo "done."
-
-echo
-echo "The repository is setup! You can now install packages."
+sudo rm /etc/apt/sources.list.d/whitewaterfoundry_pengwin-*
